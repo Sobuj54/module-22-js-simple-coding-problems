5810d4f53a450d91ce3e81d9ec5a3979edba6b7f
sobuj <sobujahmed124@gmail.com>
2023-02-01 21:27:12 +0600

    case

---
+++ case.js
@@ -0,0 +1,15 @@
+// uppercase or lower case
+
+const userName = 'blackPink'
+const userId = 'blackPinK'
+
+if (userName.toLowerCase() === userId.toLowerCase()) {
+  console.log('valid id')
+} else {
+  console.log('not valid id')
+}
+
+// converting to lowecase
+console.log(userName.toLowerCase())
+// converting to uppercase
+console.log(userId.toUpperCase())
