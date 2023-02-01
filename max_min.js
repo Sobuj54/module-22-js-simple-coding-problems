65c324df8919f8cc259d53f6a3b68628acdf4d35
sobuj <sobujahmed124@gmail.com>
2023-02-01 23:52:07 +0600

    max min

---
+++ max_min.js
@@ -0,0 +1,20 @@
+// finding max value using math.max function
+function max (a,b,c){
+    return Math.max(a,b,c)
+}
+const a = 22
+const b = 45
+const c = 21
+let maxValue = max(a,b,c)
+console.log(maxValue)
+
+// finding min value using math.min function
+
+function min (x,y,z){
+    return Math.min(x,y,z)
+}
+const x = 54
+const y = 53
+const z =22
+let minValue = min(x,y,z)
+console.log(minValue)
